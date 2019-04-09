8019bfdcf5d1ec395dc070b3bf525e6519c1c1bf
ismyblue <ismyblue@163.com>
2019-04-10 04:39:48 +0800

    初步完成login->index->自动生成menu的demo
    初步完成login->index->自动生成menu的demo

--- Practice_System/practice/views.py
+++ Practice_System/practice/views.py
@@ -4,14 +4,49 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'practice/index.html')
+    menu = {
+        '企业信息': {
+            '信息查看': 'enterprise/review/',
+            '信息修改': 'enterprise/update/',
+        },
+        '招聘管理': {
+            '岗位公开': 'enterprise/review/',
+            '审核简历': 'enterprise/update/',
+            '岗位公开1': 'enterprise/review/',
+            '审核简历2': 'enterprise/update/',
+        },
+        '实习生考核': {
+            '实习生': 'enterprise/review/',
+            '实习生记录': 'enterprise/update/',
+            '嘻嘻嘻': 'enterprise/review/',
+            '哈哈哈': 'enterprise/update/',
+        }
+    }
+    return render(request, 'practice/index.html', {'menu':menu})
 
 def login(request):
     return render(request, 'practice/login.html')
 
 def dologin(request):
-    print(request.META)
     print(request.session)
-    return redirect(reverse('practice:index'))
+    menu = {
+        '企业信息': {
+            '信息查看': 'enterprise/review/',
+            '信息修改': 'enterprise/update/',
+        },
+        '招聘管理': {
+            '岗位公开': 'enterprise/review/',
+            '审核简历': 'enterprise/update/',
+            '岗位公开1': 'enterprise/review/',
+            '审核简历2': 'enterprise/update/',
+        },
+        '实习生考核': {
+            '实习生': 'enterprise/review/',
+            '实习生记录': 'enterprise/update/',
+            '嘻嘻嘻': 'enterprise/review/',
+            '切切切': 'enterprise/update/',
+        }
+    }
+    return redirect(reverse('practice:index'),{'menu':menu})
     # return render(request, 'practice/index.html')
 
